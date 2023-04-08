65ff6e4c8e2ca109d104b8c090fedb08097013ad
TeemuKoivisto <TeemuKoivisto@users.noreply.github.com>
2023-04-08 10:16:57 +0000

    deploy: f4bd7d4077a6960b53fec18068bc7f3085db8d9b

---
+++ component---src-pages-404-tsx-cdd9475f574f618ec13b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[218],{606:function(e,t,n){n.r(t),n.d(t,{Head:function(){return l}});var s=n(2983),u=n(3667),a=n(5992);const l=()=>s.createElement(a.Z,{title:"404: Not Found"});t.default=e=>{let{data:t,location:n}=e;return s.createElement(u.A,{site:t.site},s.createElement("h1",null,"404: Not Found"),s.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-cdd9475f574f618ec13b.js.map
